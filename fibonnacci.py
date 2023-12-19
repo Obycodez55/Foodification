15217be6e44a37a425005ba7337968db0ff6012b
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-12-19 06:02:36 +0100

    First Commit- initial files. No tailwind

---
+++ fibonnacci.py
@@ -0,0 +1,7 @@
+fibonacci = []
+def fibonacciGenerator(ghy, ber):
+    area = int(ghy * ber)/2
+    print (area)
+
+# start = int(input("Start at: "))
+# length = int(input("How many terms should it have? "))
