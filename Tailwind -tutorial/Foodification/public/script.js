36858017a4a22a810ec8a24ccf50950229698564
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-12-24 20:31:32 +0100

    Updated dark mode

--- Tailwind -tutorial/Foodification/public/script.js
+++ Tailwind -tutorial/Foodification/public/script.js
@@ -7,6 +7,16 @@ $("#menu li").click(function () {
   $(this).addClass("active__nav");
 });
 
-$("#dark-mode__check").click( () => {
-    $("html").toggleClass("dark");
+$("#dark-mode__check").click(() => {
+  $("html").toggleClass("dark");
+
+  var theme = $("html").hasClass("dark");
+
+  if (theme) {
+    $("#theme-mode").text("Dark Mode");
+  } else {
+    $("#theme-mode").text("Light Mode");
+  }
 });
+
+
